f90b8117fcea792a8cd93e743156decffe330870
elijahchen <code.elijahchen@gmail.com>
2016-09-28 12:27:21 -0400

    - Refactored code to extend and implement RecyclerView - Created a custom adapter for RecyclerView (CompanyAdapter) - Added a model class for each company/product line (Company)

--- NavigationController/app/src/main/java/turntotech/org/navigationcontroller/MainActivity.java
+++ NavigationController/app/src/main/java/turntotech/org/navigationcontroller/MainActivity.java
@@ -1,21 +1,37 @@
 package turntotech.org.navigationcontroller;
 
-//import android.support.v4.app.FragmentTransaction;
-import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import turntotech.org.navigationcontroller.fragments.CompanyFragment;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import turntotech.org.navigationcontroller.utilities.Company;
+import turntotech.org.navigationcontroller.utilities.CompanyAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Company> companyArrayList;
+    CompanyAdapter companyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.thisRecyclerView);
 
-        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, new CompanyFragment()).commit();
+        Company[] CompanyArray = {
+                new Company(R.mipmap.apple_logo, "Apple"),
+                new Company(R.mipmap.apple_logo, "Samsung")
+        };
+        companyArrayList = new ArrayList<>(Arrays.asList(CompanyArray));
+
+        companyAdapter = new CompanyAdapter(companyArrayList);
+        recyclerView.setAdapter(companyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
